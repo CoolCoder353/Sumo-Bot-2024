5c57b96263752b01bcf2e32449479d0e00f69386
KablammoMan <codernaffilio@gmail.com>
2024-05-22 16:50:28 +1000

    Update sumo.ino

--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -1,16 +1,40 @@
 #include <CytronMotorDriver.h>
+
+#define SPASTICUNT_L 3
+#define PENISLICKR_R 10
+
 // Configure the motor driver.
 CytronMD motorL(PWM_DIR, 5, 4);
 CytronMD motorR(PWM_DIR, 6, 7);
 
+void findOpps()
+{
+  uint32_t start = millis();
+  while(digitalRead(SPASTICUNT_L))
+  {
+    Serial.println("While");
+    if (millis() - start > 400)
+    {
+      break;
+    }
+  }
+  Serial.println("Finished findOpps");
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(SPASTICUNT_L, INPUT); // D Pin 3
+  pinMode(PENISLICKR_R, INPUT); // D Pin 10
+
   motorL.setSpeed(255);
   motorR.setSpeed(255);
+
+  findOpps();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Running");
+  Serial.println(!digitalRead(SPASTICUNT_L));
 }
