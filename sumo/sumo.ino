c87caf11892a2eea3aa222d95a32776b658eb8d6
KablammoMan <codernaffilio@gmail.com>
2024-05-29 16:57:14 +1000

    Commented Out Main and Functions
    Got ultrasonic working

--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -1,28 +1,33 @@
-#include <CytronMotorDriver.h>
+// #include <CytronMotorDriver.h>
 
-#define SPASTICUNT_L 3
-#define PENISLICKR_R 10
+// #define SPASTICUNT_L 3
+// #define PENISLICKR_R 10
+// #define FUCKER_ECHO 8 
+// #define FUCKER_TRIG 9
 
-// Configure the motor driver.
-CytronMD motorL(PWM_DIR, 5, 4);
-CytronMD motorR(PWM_DIR, 6, 7);
+// // Configure the motor driver.
+// CytronMD motorL(PWM_DIR, 5, 4);
+// CytronMD motorR(PWM_DIR, 6, 7);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(9600);
 
-  pinMode(SPASTICUNT_L, INPUT); // D Pin 3
-  pinMode(PENISLICKR_R, INPUT); // D Pin 10
+//   pinMode(SPASTICUNT_L, INPUT); // D Pin 3
+//   pinMode(PENISLICKR_R, INPUT); // D Pin 10
+//   pinMode(FUCKER_TRIG, OUTPUT);
+//   pinMode(FUCKER_ECHO, INPUT);
 
-  motorL.setSpeed(255);
-  motorR.setSpeed(255);
+//   motorL.setSpeed(255);
+//   motorR.setSpeed(255);
 
-  findOpps();
-}
+//   // findOpps();
+// }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(!digitalRead(SPASTICUNT_L));
-}
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   // Serial.println(!digitalRead(SPASTICUNT_L)); // Is IR detecting
+//   Serial.println(penislength());
+// }
 
 
