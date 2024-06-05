b711b2d6c951b2a8ea7d556c55c999eea84e09ff
KablammoMan <codernaffilio@gmail.com>
2024-06-05 22:17:34 +1000

    Distance, Speed + General IR Function
    Utilised example HC SR04 code to create a function that returns the average speed over time interval.
    Added a function that reads an IR sensor on a port.

--- sumo/functions.ino
+++ sumo/functions.ino
@@ -1,5 +1,37 @@
-// float penislength()
-// {
-  
-//   return analogRead(FUCKER_ECHO);
-// }
+float penislength() // Ultrasonic Distance
+{
+  // Send Pulse to record data
+  digitalWrite(FUCKER_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(FUCKER_TRIG, LOW);
+  // Receive Response
+  int dura_ms = pulseIn(FUCKER_ECHO, HIGH);
+  if (dura_ms >= 38000) return -1; // Too far
+  float dist_cm = dura_ms/58;
+  if (dist_cm < 0) return -1; // Invalid Read
+  return dist_cm;
+}
+
+float cum_velocity(int time_ms) // Get Average Speed (over 60ms increments/intervals)
+{
+  float firstd = 0;
+  float finald = 0;
+  float start = millis();
+  float dist = 0;
+  while (millis() - start < time_ms)
+  {
+    dist = penislength();
+    if (dist < 0) continue;
+    if (firstd <= 0) firstd = dist;
+    finald = dist;
+    delay(60);
+  }
+  float ellapsed = millis() - start;
+  float velocity = (finald-firstd)/ellapsed;
+  return velocity;
+}
+
+bool somethingsInMyWay(uint8_t port) // IR Sensor
+{
+  return !digitalRead(port);
+}
--- sumo/testultrasonic.ino
+++ sumo/testultrasonic.ino
@@ -1,35 +1,35 @@
-int trig=9;
-int echo=8;
-int duration;
-float distance;
-float meter;
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(trig, OUTPUT);
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  pinMode(echo, INPUT);
-  delay(6000);
-  Serial.println("Distance:");
-}
-void loop()
-{
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  duration = pulseIn(echo, HIGH);
-  if(duration>=38000){
-    Serial.print("Out range");
-  }
-  else{
-    distance = duration/58;
-    Serial.print(distance);
-    Serial.print("cm");
-    meter=distance/100;
-    Serial.print("\t");
-    Serial.print(meter);
-    Serial.println("m");
-  }
-  delay(80);
-}
+// int trig=9;
+// int echo=8;
+// int duration;
+// float distance;
+// float meter;
+// void setup()
+// {
+//   Serial.begin(9600);
+//   pinMode(trig, OUTPUT);
+//   digitalWrite(trig, LOW);
+//   delayMicroseconds(2);
+//   pinMode(echo, INPUT);
+//   delay(6000);
+//   Serial.println("Distance:");
+// }
+// void loop()
+// {
+//   digitalWrite(trig, HIGH);
+//   delayMicroseconds(10);
+//   digitalWrite(trig, LOW);
+//   duration = pulseIn(echo, HIGH);
+//   if(duration>=38000){
+//     Serial.print("Out range");
+//   }
+//   else{
+//     distance = duration/58;
+//     Serial.print(distance);
+//     Serial.print("cm");
+//     meter=distance/100;
+//     Serial.print("\t");
+//     Serial.print(meter);
+//     Serial.println("m");
+//   }
+//   delay(60);
+// }
