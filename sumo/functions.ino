80fcef129e380a4f58fde036a6c3f7c044622827
CoolCoder64 <GitHub@otmfamily.com>
2024-06-06 16:34:07 +1000

    Renamed everything
    So I can understand it

--- sumo/functions.ino
+++ sumo/functions.ino
@@ -1,18 +1,20 @@
-float penislength() // Ultrasonic Distance
+float GetDistance() // Ultrasonic Distance
 {
   // Send Pulse to record data
-  digitalWrite(FUCKER_TRIG, HIGH);
+  digitalWrite(ULTRASONIC_TRIG, HIGH);
   delayMicroseconds(10);
-  digitalWrite(FUCKER_TRIG, LOW);
+  digitalWrite(ULTRASONIC_TRIG, LOW);
   // Receive Response
-  int dura_ms = pulseIn(FUCKER_ECHO, HIGH);
-  if (dura_ms >= 38000) return -1; // Too far
-  float dist_cm = dura_ms/58;
-  if (dist_cm < 0) return -1; // Invalid Read
+  int dura_ms = pulseIn(ULTRASONIC_ECHO, HIGH);
+  if (dura_ms >= 38000)
+    return -1; // Too far
+  float dist_cm = dura_ms / 58;
+  if (dist_cm < 0)
+    return -1; // Invalid Read
   return dist_cm;
 }
 
-float cum_velocity(int time_ms) // Get Average Speed (over 60ms increments/intervals)
+float GetVelocity(int time_ms) // Get Average Speed (over 60ms increments/intervals)
 {
   float firstd = 0;
   float finald = 0;
@@ -20,18 +22,20 @@ float cum_velocity(int time_ms) // Get Average Speed (over 60ms increments/inter
   float dist = 0;
   while (millis() - start < time_ms)
   {
-    dist = penislength();
-    if (dist < 0) continue;
-    if (firstd <= 0) firstd = dist;
+    dist = GetDistance();
+    if (dist < 0)
+      continue;
+    if (firstd <= 0)
+      firstd = dist;
     finald = dist;
     delay(60);
   }
   float ellapsed = millis() - start;
-  float velocity = (finald-firstd)/ellapsed;
+  float velocity = (finald - firstd) / ellapsed;
   return velocity;
 }
 
-bool somethingsInMyWay(uint8_t port) // IR Sensor
+bool isSomethingInFront(uint8_t port) // IR Sensor
 {
   return !digitalRead(port);
 }
--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -1,24 +1,25 @@
 #include <CytronMotorDriver.h>
 
-#define SPASTICUNT_L 3
-#define PENISLICKR_R 10
-#define FUCKER_ECHO 8 
-#define FUCKER_TRIG 9
-#define FUCKER_TIMEOUT 300
-#define COCAINE_LINE1 
+#define MOTOR_L 3
+#define MOTOR_R 10
+#define ULTRASONIC_ECHO 8
+#define ULTRASONIC_TRIG 9
+#define ULTRASONIC_TIMEOUT 300
+#define COCAINE_LINE1
 
 // Configure the motor driver.
 CytronMD motorL(PWM_DIR, 5, 4);
 CytronMD motorR(PWM_DIR, 6, 7);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  pinMode(SPASTICUNT_L, INPUT); // D Pin 3
-  pinMode(PENISLICKR_R, INPUT); // D Pin 10
-  pinMode(FUCKER_TRIG, OUTPUT);
-  pinMode(FUCKER_ECHO, INPUT);
+  pinMode(MOTOR_L, INPUT); // D Pin 3
+  pinMode(MOTOR_R, INPUT); // D Pin 10
+  pinMode(ULTRASONIC_TRIG, OUTPUT);
+  pinMode(ULTRASONIC_ECHO, INPUT);
 
   motorL.setSpeed(255);
   motorR.setSpeed(255);
@@ -26,10 +27,9 @@ void setup() {
   // findOpps();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   // Serial.println(!digitalRead(SPASTICUNT_L)); // Is IR detecting
-  Serial.println(penislength());
+  Serial.println(GetDistance());
 }
-
-
